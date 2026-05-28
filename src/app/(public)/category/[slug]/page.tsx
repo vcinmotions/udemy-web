89c977c9e217ccc99be4c0df1a86f3dfd1856aa3
vcinmotions <shobha@vcinmotions.com>
2026-05-28 15:49:45 +0530

    Fixed UI

--- src/app/(public)/category/[slug]/page.tsx
+++ src/app/(public)/category/[slug]/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import CategoryContent from "../components/CategoryContent";
 
 export default function CategoryPage() {
   return (
     <div className="min-h-screen bg-background">
-      <main>
-        <CategoryContent />
-      </main>
-    </div>
+          <main className="pt-16">
+            <Suspense fallback={<div>Loading...</div>}>
+            <CategoryContent />
+            </Suspense>
+          </main>
+        </div>
   );
 }
